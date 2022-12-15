baf637fbb5762ed1f73948e455c166fd64c94d78
inksnw <inksnw@gmail.com>
2022-12-15 20:49:44 +0800

    日志行号

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use tokio;
+use tracing::*;
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() {
+    tracing_subscriber::fmt().event_format(
+        tracing_subscriber::fmt::format()
+            .with_file(true)
+            .with_line_number(true)
+    ).init();
+
+    info!("Preparing kubelet config.");
     println!("Hello, world!");
 }
